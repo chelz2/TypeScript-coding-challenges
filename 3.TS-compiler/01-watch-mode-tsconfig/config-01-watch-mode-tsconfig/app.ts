1c0f84ad7153834c29f79d6ab52a34e87c69070a
chelz2 <golirida@hotmail.com>
2025-03-05 04:05:40 +0530

    tsc configuration & options- compilation target, core libs, source map, outdir/rootdir, stop emit, code quality, debugger options

--- 3.TS-compiler/01-watch-mode-tsconfig/config-01-watch-mode-tsconfig/app.ts
+++
@@ -1,3 +0,0 @@
-const userName = "Maxy";
-
-console.log(userName);
---
+++ 3.TS-compiler/01-watch-mode-tsconfig/config-01-watch-mode-tsconfig/src/app.ts
@@ -0,0 +1,7 @@
+const button = document.querySelector("button")!;
+
+button.addEventListener("click", () => {
+  console.log("C l i c k e d !");
+});
+
+console.log("button");
