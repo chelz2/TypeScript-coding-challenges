90278c8a15d08095aa64a1f8143658b2e57630de
chelz2 <golirida@hotmail.com>
2025-03-18 09:24:23 +0530

    decorator code exec- class defn & class initn, method decorator, solving problem - this bind to method decorator

--- 8.DecoratorsECMAScripts/ECMADecorators.ts
+++ 8.DecoratorsECMAScripts/ECMADecorators.ts
@@ -1,5 +1,7 @@
 // ECMAScript Decorators -- OOP class method & properties features only
 
+//1. Class Decorators
+
 // building my first decorators
 
 function logger(target: any, ctx: ClassDecoratorContext) {
@@ -24,7 +26,9 @@ function logger2<T extends new (...args: any[]) => any>(
   target: T,
   ctx: ClassDecoratorContext
 ) {
-  console.log("Logger Decorator - returns new class extra info");
+  console.log(
+    "Logger Decorator - returning new class object with new properties"
+  );
 
   return class extends target {
     age = 44;
@@ -33,7 +37,7 @@ function logger2<T extends new (...args: any[]) => any>(
 }
 
 @logger2 // adds extra info to Class
-class Man {
+class Person1 {
   name = "John";
 
   greet() {
@@ -41,6 +45,74 @@ class Man {
   }
 }
 
-const person1 = new Man();
+const person1 = new Person1();
 
 console.log(person1);
+
+// decorator execution order
+
+function logger3<T extends new (...args: any[]) => any>(
+  target: T,
+  ctx: ClassDecoratorContext
+) {
+  console.log("Logger Decorator");
+  console.log(target, ctx);
+
+  return class extends target {
+    class = "XCV";
+    constructor(...args: any[]) {
+      super(...args);
+      console.log("class constructor");
+      console.log(this);
+    }
+  };
+}
+
+@logger3
+class Person2 {
+  name = "Christy";
+
+  greet() {
+    console.log("Hello I am " + this.name);
+  }
+}
+
+const christy = new Person2();
+
+//2. Method Decorators
+
+//creating my first class method decorator
+
+function autobind(
+  target: (...args: any[]) => any,
+  ctx: ClassMethodDecoratorContext
+) {
+  ctx.addInitializer(function (this: any) {
+    this[ctx.name] = this[ctx.name].bind();
+  });
+}
+
+@logger3
+class Person3 {
+  name = "Monday";
+
+  // constructor() {
+  //   this.greet = this.greet.bind(this);
+  // }
+  @autobind
+  greet() {
+    console.log("Hello I am " + this.name);
+  }
+}
+
+const monday = new Person3();
+
+// solving common problems
+
+monday.greet(); // exec greet method
+
+const greet = monday.greet; // pointer - to greet method
+//greet(); // !!!ERROR - without "this" bind added to class manually
+//greet();
+
+greet(); // "this" bind added to decorator itself - dynamic can be use on any class method
